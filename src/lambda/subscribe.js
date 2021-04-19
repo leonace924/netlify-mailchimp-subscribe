0acfc1602c348d82529587fb112d09de7554bcb1
aceberry924 <meobius123@gmail.com>
2021-04-19 14:58:24 -0400

    Update API call

--- src/lambda/subscribe.js
+++ src/lambda/subscribe.js
@@ -1,10 +1,10 @@
-const fetch = require("node-fetch");
+const request = require("request");
 
 const mailChimpAPI = process.env.MAILCHIMP_API_KEY;
 const mailChimpListID = process.env.MAILCHIMP_LIST_ID;
 const mcRegion = process.env.MAILCHIMP_REGION;
 
-module.exports.handler = async (event, context, callback) => {
+module.exports.handler = (event, context, callback) => {
   const formData = JSON.parse(event.body);
   const email = formData.email;
   let errorMessage = null;
@@ -27,45 +27,49 @@ module.exports.handler = async (event, context, callback) => {
     callback(errorMessage);
   }
 
-  try {
-    const subscriber = {
-      email_address: email,
-      status: "subscribed",
-      merge_fields: {},
-    };
+  const data = {
+    email_address: email,
+    status: "subscribed",
+    merge_fields: {},
+  };
 
-    const response = await fetch(
-      `https://${mcRegion}.api.mailchimp.com/3.0/lists/${mailChimpListID}/members`,
-      {
-        method: "POST",
-        body: JSON.stringify(subscriber),
-        headers: {
-          Authorization: `apikey ${mailChimpAPI}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
+  const subscriber = JSON.stringify(data);
 
-    const data = await response.json();
-
-    if (!response.ok) {
-      // NOT res.status >= 200 && res.status < 300
-      callback(data.detail, null);
-    }
-
-    callback(null, {
-      statusCode: 201,
+  request(
+    {
+      method: "POST",
+      url: `https://${mcRegion}.api.mailchimp.com/3.0/lists/${mailChimpListID}/members`,
+      body: subscriber,
       headers: {
+        Authorization: `apikey ${mailChimpAPI}`,
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Credentials": "true",
       },
-      body: JSON.stringify({
-        status: "saved email",
-      }),
-    });
-  } catch (err) {
-    console.log(err); // output to netlify function log
-    callback(err.message, null);
-  }
+    },
+    (error, response, body) => {
+      if (error) {
+        callback(error, null);
+      }
+      const bodyObj = JSON.parse(body);
+
+      if (
+        response.statusCode < 300 ||
+        (bodyObj.status === 400 && bodyObj.title === "Member Exists")
+      ) {
+        callback(null, {
+          statusCode: 201,
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": "true",
+          },
+          body: JSON.stringify({
+            status: "saved email",
+          }),
+        });
+      } else {
+        console.log("Error from mailchimp", bodyObj.detail);
+        callback(bodyObj.detail, null);
+      }
+    }
+  );
 };
